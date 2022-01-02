3d13178004d119600aa2035d1aef8b2fa380a88a
= <=>
2022-01-02 19:42:42 +0500

    123

--- vue-lesson/src/App.vue
+++ vue-lesson/src/App.vue
@@ -1,13 +1,17 @@
 <template>
-	<div>
-
-	</div>
+  <div>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore iure
+      perferendis nisi. Non harum culpa deleniti deserunt eligendi maiores?
+      Explicabo sint totam dolore excepturi accusantium delectus obcaecati
+      repellat id dolorum?
+    </p>
+    <button class="btn btn-success bg-danger">Hello world</button>
+  </div>
 </template>
 
 <script>
-
 </script>
 
 <style>
-	
 </style>

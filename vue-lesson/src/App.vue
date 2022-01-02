6c32855154746480ff85ed967b651b997d339aa1
= <=>
2022-01-02 21:58:06 +0500

    Vue js

--- vue-lesson/src/App.vue
+++ vue-lesson/src/App.vue
@@ -1,17 +1,68 @@
 <template>
-  <div>
-    <p>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore iure
-      perferendis nisi. Non harum culpa deleniti deserunt eligendi maiores?
-      Explicabo sint totam dolore excepturi accusantium delectus obcaecati
-      repellat id dolorum?
-    </p>
-    <button class="btn btn-success bg-danger">Hello world</button>
+  <div class="container">
+    <div class="form mt-4">
+      <form>
+        <input
+          :value="name"
+          type="text"
+          placeholder="Enter your name..."
+          class="form-control"
+        />
+        <input
+          :value="email"
+          type="text"
+          placeholder="Enter your email..."
+          class="form-control my-4"
+        />
+        <input
+          :value="content"
+          type="text"
+          placeholder="Enter your contents..."
+          class="form-control"
+        />
+        <div class="d-flex flex-row-reverse">
+          <button type="submit" class="btn btn-primary mt-2">
+            Add Comments
+          </button>
+        </div>
+      </form>
+    </div>
+    <div class="row align-items-center justify-content-center">
+      <div
+        class="col-md-3 border border-danger mr-2"
+        v-for="comment in comments"
+      >
+        <p><b>Name: </b>{{ comment.name }}</p>
+        <p><b>Email: </b>{{ comment.email }}</p>
+        <p><b>Content: </b>{{ comment.content }}</p>
+      </div>
+    </div>
   </div>
-</template>
+</template> 
 
 <script>
+export default {
+  data() {
+    return {
+      comments: [
+        { id: 1, name: "Home", email: "anvar@gmail.com", content: "123" },
+        { id: 2, item: "About", email: "home@gmail.com", content: "dvkjdbv" },
+      ],
+      name: "",
+      email: "",
+      content: "",
+    };
+  },
+  methods: {
+    inputName(event) {
+      this;
+    },
+  },
+};
 </script>
 
 <style>
+.begemot {
+  width: 150px;
+}
 </style>
